8fd9a68555ad2cf213989ddf5f97b01b31b51554
gbertoldo <glbertoldo@gmail.com>
2024-07-25 10:11:35 -0300

    Adds data for verification, and changes button class for compatibility with hRocket  hardware  1.1.0.

--- src/Button.cpp
+++ src/Button.cpp
@@ -22,103 +22,90 @@
   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
-
 #include "Arduino.h"
 #include "Button.h"
 
 bool Button::begin(unsigned short pin, unsigned long int refTime)
 {
 
-  pinMode(pin, INPUT);
+  pinMode(pin, INPUT_PULLUP);
 
   this->pin = pin;
 
   this->refTime = refTime;
-
+  
   return true;
 }
 
 
 ButtonState Button::getState()
-{
+{ 
 
-  // Reading the current pin state (pressed=1 or released=0)
-  cstate = digitalRead(pin);
+    // Reading the current pin state (pressed=ON or released=OFF)
+    cstate = digitalRead(pin);
 
-  // If button is currently realeased
-  if ( cstate == 0 )
-  {
+    // If button is currently realeased
+    if ( cstate == OFF ) {
 
-    // If button was released before
-    if ( pstate == 0 )
-    {
+        // If button was released before
+        if ( pstate == OFF ) {
 
-      pstate = cstate;
+            pstate = cstate;
 
-      return ButtonState::released;
+            return ButtonState::released;
+            
+        } 
+        // If button was pressed before
+        else 
+        {
+        
+            pstate = cstate;
 
+            unsigned long int T1 = millis()-T0;
+
+            // If short pressed
+            if ( 5 < T1 && T1 < refTime ) {
+
+                return ButtonState::pressedAndReleased;
+              
+            }
+            else
+            {
+                return ButtonState::released;
+            }               
+        }
     }
-    // If button was pressed before
+    // If button is currently pressed
     else
     {
+        // If button was released before
+        if ( pstate == OFF ) {
 
-      pstate = cstate;
+            pstate = cstate;
 
-      unsigned long int T1 = millis() - T0;
+            T0 = millis();
 
-      // If short pressed
-      if ( 5 < T1 && T1 < refTime )
-      {
+            return ButtonState::pressed;
+            
+        } 
+        // If button was pressed before
+        else 
+        {
+        
+            pstate = cstate;
 
-        return ButtonState::pressedAndReleased;
+            if ( (millis()-T0) >= refTime ) {
 
-      }
-      else
-      {
-
-        return ButtonState::released;
-
-      }
-
+                return ButtonState::longPressed;
+              
+            }
+            else
+            {
+                return ButtonState::pressed;
+            }
+                            
+        }
+        
     }
-
-  }
-  // If button is currently pressed
-  else
-  {
-
-    // If button was released before
-    if ( pstate == 0 )
-    {
-
-      pstate = cstate;
-
-      T0 = millis();
-
-      return ButtonState::pressed;
-
-    }
-    // If button was pressed before
-    else
-    {
-
-      pstate = cstate;
-
-      if ( (millis() - T0) >= refTime )
-      {
-
-        return ButtonState::longPressed;
-
-      }
-      else
-      {
-
-        return ButtonState::pressed;
-
-      }
-
-    }
-
-  }
-
+  
 };
--- src/Button.h
+++ src/Button.h
@@ -22,21 +22,19 @@
   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
-
 #ifndef BUTTON_H
 #define BUTTON_H
 
-
 /*
   Button states
-              released: at time button state was read, it was released
+              released: at time button state was read, it was released 
                pressed: at time button state was read, it was pressed by a time less than refTime (user defined reference time)
     pressedAndReleased: at time button state was read, it was released after had been pressed by a time less than refTime (user defined reference time)
            longPressed: at time button state was read, it was pressed by a time greater or equal to refTime (user defined reference time)
-*/
-
+*/ 
 
 enum class ButtonState {released, pressed, pressedAndReleased, longPressed};
+
 
 
 /*
@@ -48,31 +46,34 @@ enum class ButtonState {released, pressed, pressedAndReleased, longPressed};
 class Button
 {
 
-  public:
+    public:
+        // Initializes
+        bool begin(unsigned short pin, unsigned long int refTime = 1500);
 
-    // Initializes
-    bool begin(unsigned short pin, unsigned long int refTime = 1500);
+        // Returns the button state
+        ButtonState getState();
 
-    // Returns the button state
-    ButtonState getState();
+    private:
 
-  private:
+        // Defines on and off
+        uint8_t  ON  {LOW}; // pressed
+        uint8_t OFF {HIGH}; // released
 
-    // Arduino pin associated to the button
-    unsigned short pin;
+        // Arduino pin associated to the button
+        unsigned short pin;
 
-    // Reference time to long pressed button state (milliseconds)
-    unsigned long int refTime {1500};
+        // Reference time to long pressed button state (milliseconds)
+        unsigned long int refTime {1500};
 
-    // Previous button state (pressed=1 or released=0)
-    unsigned short pstate{0};
+        // Previous button state (pressed=ON or released=OFF)
+        unsigned short pstate{OFF};
+        
+        // Current button state (pressed=ON or released=OFF)
+        unsigned short cstate{OFF};
 
-    // Current button state (pressed=1 or released=0)
-    unsigned short cstate{0};
-
-    // Stores the time when button was first pressed after a release
-    unsigned long int T0;
-
+        // Stores the time when button was first pressed after a release
+        unsigned long int T0;
+          
 };
 
 #endif // BUTTON_H
